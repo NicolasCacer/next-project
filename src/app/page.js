0c055fd1fff46c666586fbfea039562ee861743b
Nicolas <njcaceres04@gmail.com>
2024-11-28 17:43:00 -0500

    First commit

--- src/app/page.js
+++ src/app/page.js
@@ -47,6 +47,7 @@ export default function Home() {
           >
             Read our docs
           </a>
+          <a href='/dashboard'>Dashboard</a>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
