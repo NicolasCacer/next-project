df507a727f19a7c52d3d99af0a96edae5c0a9872
Nicolas <njcaceres04@gmail.com>
2024-11-28 18:30:40 -0500

    First develop commit

--- src/app/(pages)/dashboard/page.js
+++ src/app/(pages)/dashboard/page.js
@@ -5,7 +5,7 @@ export default function Dashboard() {
   const router = useRouter();
   return (
     <div>
-      <button className="bg-purple-500 shadow-xl p-2 rounded-full hover:bg-purple-800" onClick={()=>router.push('/')}>Go to main</button>
+      <button className="bg-purple-500 shadow-xl p-2 rounded-full hover:bg-purple-800" onClick={()=>router.push('/')}>Go to Main menu</button>
     </div>
   );
 }
